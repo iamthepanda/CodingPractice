5a7fe0d6e2745a66caa4526285024dc2d90aec74
Darin Vergara <darinvergara@gmail.com>
2016-01-31 19:51:48 -0800

    added basic header file

---
+++ remove-duplicates.h
@@ -0,0 +1,12 @@
+// Write code to remove duplicates from an unsorted linked list
+
+class LinkedList {
+private:
+  int data;
+  //define struct here
+  bool isDuplicate();
+public:
+
+  void removeDuplicates();
+  //remove duplicates
+}
